acce7de7ee594324939a5c8a6425607b73ba2b59
OLGA <1986o2019@mail.ru>
2023-07-09 01:05:05 +0300

    Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > b && a > c)
+    Console.Write("max: " + a);
+if (b > a && b > c)
+    Console.Write("max: " + b);
+else
+    Console.Write("max: " + c);
