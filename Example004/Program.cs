0dca1018b855fb1c67305abc2745f70438a31dd1
OLGA <1986o2019@mail.ru>
2023-07-09 01:34:26 +0300

    Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < n1; i++)
+if (n1 % 2 == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+    
